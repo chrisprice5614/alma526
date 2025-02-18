0cade167443076b4720fd2bdc07627f1e856a1b9
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2025-02-18 14:26:11 -0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+fetch('/nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#nav_replace");
+    let newelem = document.createElement("div");
+    newelem.className = "noMarge";
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
