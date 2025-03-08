8e5f82f313ca35ed9f0ffbd384819ce8e0c72b9d
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2025-03-08 04:45:44 -0800

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,8 @@
+var items = [
+    {
+        title: "A Mission of Transformation: Clay Smith’s Journey of Faith and Humility",
+        link: "/blog/clay/blog.html",
+        img: "/blog/clay/hero.jpg"
+    },
+
+]
---
+++ canyoufeel.js
@@ -0,0 +1,9 @@
+var items = [
+    {
+        title: "Clay Smith",
+        link: "/transcripts/clay/canclaysmith.html",
+        img: "/blog/clay/hero.jpg",
+        spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1cetiVjcDv11sOTk6ReZQZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    },
+
+]
