bf079aa9b4021f715e1fd81c815994544b4c4e22
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2025-03-10 17:51:35 -0700

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,6 +1,11 @@
 var items = [
     {
         title: "A Mission of Transformation: Clay Smith’s Journey of Faith and Humility",
+        link: "/blog/rachels/blog.html",
+        img: "/blog/rachels/image001.jpg"
+    },
+    {
+        title: "A Mission of Transformation: Clay Smith’s Journey of Faith and Humility",
         link: "/blog/clay/blog.html",
         img: "/blog/clay/hero.jpg"
     },
--- canyoufeel.js
+++ canyoufeel.js
@@ -1,9 +1,17 @@
 var items = [
     {
+        title: "Rachel Stumm",
+        link: "/transcripts/canrachels.html",
+        img: "/transcripts/canrachels.jpg",
+        spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/3mpWdo2jOwRSLvfnV3N1UM?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    },
+    {
         title: "Clay Smith",
         link: "/transcripts/clay/canclaysmith.html",
         img: "/blog/clay/hero.jpg",
         spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1cetiVjcDv11sOTk6ReZQZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
 
+
+    
 ]
