5ec9761a5aad1b6df5ac1912d1aaae2fc80a8859
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2025-03-15 05:12:42 -0700

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,5 +1,15 @@
 var items = [
     {
+        title: "Embracing Change: Tanner Johnson's Inspiring Journey Through Missionary Work",
+        link: "/blog/tannerj/blog.html",
+        img: "/blog/tannerj/0.jpg"
+    },
+    {
+        title: "Embracing the Call: Sister Nora Campbell's Inspiring Journey to Missionary Service",
+        link: "/blog/noracam/blog.html",
+        img: "/blog/noracam/0.jpg"
+    },
+    {
         title: "A Mission of Transformation: Clay Smith’s Journey of Faith and Humility",
         link: "/blog/rachels/blog.html",
         img: "/blog/rachels/image001.jpg"
---
+++ transcripts.js
@@ -0,0 +1,23 @@
+var items = [
+    {
+        title: "Nora Campbell",
+        link: "/transcripts/noracam.html",
+        img: "/blog/noracam/0.jpg",
+        spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/7uoUC5M8E9sm5Zu34HUVEq?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+        type: "cttw"
+    },
+    {
+        title: "Rachel Stumm",
+        link: "/transcripts/canrachels.html",
+        img: "/transcripts/canrachels.jpg",
+        spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/3mpWdo2jOwRSLvfnV3N1UM?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+        type: "cyfsn"
+    },
+    {
+        title: "Clay Smith",
+        link: "/transcripts/clay/canclaysmith.html",
+        img: "/blog/clay/hero.jpg",
+        spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1cetiVjcDv11sOTk6ReZQZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+        type: "cyfsn"
+    },
+]
